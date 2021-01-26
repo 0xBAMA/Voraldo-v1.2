926991289fa20735e4663b424ebab0bc63b727a5
0xBAMA <jon.baker1222@gmail.com>
2021-01-25 22:34:56 -0500

    added debug stuff to the gl_debug_enable()

--- resources/debug/debug.h
+++ resources/debug/debug.h
@@ -42,7 +42,7 @@ void gl_debug_enable()
   cout << "OpenGL debug output enabled." << endl << endl;
 
 
-  //report all gl extensions - useful on different platforms
+  //report all gl extensions - maybe useful on different platforms
 
   // GLint n;
   //
@@ -55,9 +55,8 @@ void gl_debug_enable()
   // cout << endl;
 
 
-
-
   //gl info re:texture size, texture units
+  // querying the things that might cause issues with the drivers - so far this has not given me any real idea of what's going on
 
   // GLint val;
   // glGetIntegerv(GL_MAX_TEXTURE_SIZE, &val);
@@ -66,8 +65,43 @@ void gl_debug_enable()
   // glGetIntegerv(GL_MAX_3D_TEXTURE_SIZE, &val);
   // cout << "max 3dtexture size reports: " << val << " on all 3 edges" << endl << endl;
   //
-  // glGetIntegerv(GL_MAX_COMPUTE_TEXTURE_IMAGE_UNITS, &val);
-  // cout << "max compute texture image units reports: " << val << endl << endl;
+  
+  // if (glewIsSupported("GL_EXT_shader_image_load_store"))
+  //     cout << "GL_EXT_shader_image_load_store is supported" << endl;
+  // else
+  //     cout << "GL_EXT_shader_image_load_store is not supported" << endl;
+
+  // GLint textureCount;
+  // glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &textureCount);
+  // cout << endl << "GL_MAX_TEXTURE_IMAGE_UNITS returned:" << textureCount << endl << endl;
+
+  // glGetIntegerv(GL_MAX_COMPUTE_TEXTURE_IMAGE_UNITS, &textureCount);
+  // cout << endl << "GL_MAX_COMPUTE_TEXTURE_IMAGE_UNITS returned:" << textureCount << endl << endl;
+
+  // glGetIntegerv(GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, &textureCount);
+  // cout << endl << "GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS returned:" << textureCount << endl << endl;
+
+  // glGetIntegerv(GL_MAX_3D_TEXTURE_SIZE, &textureCount);
+  // cout << endl << "GL_MAX_3D_TEXTURE_SIZE returned:" << textureCount << endl << endl;
+
+  /* GLint data[3]; */
+
+  /* glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_COUNT,0, &data[0]); */
+  /* glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_COUNT,1, &data[1]); */
+  /* glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_COUNT,2, &data[2]); */
+  /* cout << endl << "GL_MAX_COMPUTE_WORK_GROUP_COUNT returned x:" << data[0] << " y:" << data[1] << " z:" << data[2] << endl; */
+
+  /* glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_SIZE,0, &data[0]); */
+  /* glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_SIZE,1, &data[1]); */
+  /* glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_SIZE,2, &data[2]); */
+  /* cout << endl << "GL_MAX_COMPUTE_WORK_GROUP_SIZE returned x:" << data[0] << " y:" << data[1] << " z:" << data[2] << endl; */
+
+  /* GLint max; */
+  /* glGetIntegerv(  GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max); */
+  /* cout << endl << "GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS returned: " << max << endl << endl; */
+
+  // glGetIntegerv(  GL_MAX_COMPUTE_SHARED_MEMORY_SIZE, &max);
+  // cout << endl << "GL_MAX_COMPUTE_SHARED_MEMORY_SIZE returned: " << max << " bytes" << endl << endl; 
 }
 
 #endif
