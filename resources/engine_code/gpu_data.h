a27aebf582d358c7038baed57af520ad1d56f0e0
jb <jon.baker1222@gmail.com>
2021-02-07 08:03:08 -0500

    actually fixed

--- resources/engine_code/gpu_data.h
+++ resources/engine_code/gpu_data.h
@@ -3,56 +3,51 @@
 
 #include "includes.h"
 
-class GLContainer
-{
-    public:
+class GLContainer {
+public:
+  GLContainer() {}
+  ~GLContainer() {}
 
-        GLContainer()  {}
-        ~GLContainer() {}
+  // GPU side initialization
+  void init() {
+    // compile_shaders();
+    // buffer_geometry();
+    // load_textures();
+  }
 
-        // GPU side initialization
-        void init()
-        {
-            // compile_shaders();
-            // buffer_geometry();
-            // load_textures();
-        } 
+  // display functions
+  bool show_widget = true;
+  void display() {
+    display_block();
+    if (show_widget)
+      display_orientation_widget();
+  }
 
-        // display functions
-        bool show_widget = true;
-        void display()
-        {
-            display_block();
-            if(show_widget)
-                display_orientation_widget();
-        }
+  // OpenGL clear color
+  glm::vec4 clear_color;
 
-        // OpenGL clear color
-        glm::vec4 clear_color; 
+  // screen dimensions
+  unsigned int screen_width, screen_height;
 
-        // screen dimensions
-        unsigned int screen_width, screen_height; 
+private:
+  // display helper functions
+  void display_block();
+  void display_orientation_widget();
 
-    private:
-        // display helper functions
-        void display_block();
-        void display_orientation_widget();
+  // // OpenGL Data
+  //  // blitting via fullscreen geometry
+  //  GLuint display_vao, display_vbo,
+  //      display_shader_program =
+  //          Shader("resources/engine_code/shaders/blit.vs.glsl",
+  //                  "resources/engine_code/shaders/blit.fs.glsl")
+  //              .Program;
 
-       // OpenGL Data
-        // blitting via fullscreen geometry
-        GLuint display_vao, display_vbo,
-            display_shader_program =
-                Shader("resources/engine_code/shaders/blit.vs.glsl",
-                        "resources/engine_code/shaders/blit.fs.glsl")
-                    .Program;
-
-          // orientation widget with phong shading
-          GLuint owidget_vao, owidget_vbo,
-              owidget_shader_program =
-                  Shader("resources/engine_code/shaders/widget.vs.glsl",
-                         "resources/engine_code/shaders/widget.fs.glsl")
-                      .Program;
-
+  //    // orientation widget with phong shading
+  //    GLuint owidget_vao, owidget_vbo,
+  //        owidget_shader_program =
+  //            Shader("resources/engine_code/shaders/widget.vs.glsl",
+  //                   "resources/engine_code/shaders/widget.fs.glsl")
+  //                .Program;
 };
 
 #endif
