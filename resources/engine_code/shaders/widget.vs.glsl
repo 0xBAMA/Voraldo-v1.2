310778e3582129bd3e7c5473acef580f9eec5f24
jb <jon.baker1222@gmail.com>
2021-02-11 17:04:47 -0500

    remove old code from widget shader

--- resources/engine_code/shaders/widget.vs.glsl
+++ resources/engine_code/shaders/widget.vs.glsl
@@ -16,33 +16,13 @@ uniform vec3 basis_x;
 uniform vec3 basis_y;
 uniform vec3 basis_z;
 
-
 uniform vec3 offset;
-
-//thanks to Neil Mendoza via http://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
-// mat3 rotationMatrix(vec3 axis, float angle)
-// {
-//     axis = normalize(axis);
-//     float s = sin(angle);
-//     float c = cos(angle);
-//     float oc = 1.0 - c;
-
-//     return mat3(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,
-//                 oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,
-//                 oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c);
-// }
 
 
 void main()
 {
-
-	// mat3 rotphi = rotationMatrix(vec3(1,0,0), -1.0*phi);
-	// mat3 rottheta = rotationMatrix(vec3(0,1,0), -1.0*theta);
-
-	// normal = rotphi * rottheta * vNormal;
 	normal = basis_x * vNormal.x + basis_y * vNormal.y + basis_z * vNormal.z;
 	color = vColor;
-	// vpos = 0.2 * (rotphi * rottheta * vPosition);
 	vpos = 0.2 * (basis_x * vPosition.x + basis_y * vPosition.y + basis_z * vPosition.z);
 	
 	vpos.x /= ratio;
