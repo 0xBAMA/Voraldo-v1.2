fd27007c85640496e8a3427310ca2ee4112913be
jb <jonbaker1222@gmail.com>
2021-12-28 10:26:10 -0600

    add init script

--- resources/engine_code/engine_utils.cc
+++ resources/engine_code/engine_utils.cc
@@ -189,7 +189,8 @@ void engine::SDL2_setup() {
   total_screen_width = dm.w;
   total_screen_height = dm.h;
 
-  window = SDL_CreateWindow( "Voraldo v1.2", total_screen_width, 0, total_screen_width, total_screen_height, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN | SDL_WINDOW_BORDERLESS );
+  // window = SDL_CreateWindow( "Voraldo v1.2", total_screen_width, 0, total_screen_width, total_screen_height, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN | SDL_WINDOW_BORDERLESS );
+  window = SDL_CreateWindow( "Voraldo v1.2", 0, 0, total_screen_width, total_screen_height, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN | SDL_WINDOW_BORDERLESS );
   SDL_ShowWindow(window);
 #else
   total_screen_width = dm.w * 3;
